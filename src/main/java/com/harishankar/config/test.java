1edafe7d0090b06ac7d6e36fea4cdf289406b353
Harishankar Sah <154059263+saha2shankar@users.noreply.github.com>
2025-08-15 21:56:17 +0545

    password protect

--- src/main/java/com/harishankar/config/test.java
+++ src/main/java/com/harishankar/config/test.java
@@ -1,14 +1,5 @@
 package com.harishankar.config;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 public class test {
-    public static void main(String[] args) {
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String rawPassword = "harishankar";
-        String encodedPassword = passwordEncoder.encode(rawPassword);
-
-        System.out.println("Encoded password: " + encodedPassword);
-    }
+  
 }
